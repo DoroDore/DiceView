caaebd65bc7fa5da102b45954264120c23ca5cc2
DoroDore <tlyz82@student.cis.eduhk>
2025-05-22 09:24:39 +0800

    Initial Commit

---
+++ DiceView/DiceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewApp.swift
+//  DiceView
+//
+//  Created by Theodore Zhu on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceViewTests/DiceViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  DiceViewTests.swift
+//  DiceViewTests
+//
+//  Created by Theodore Zhu on 5/22/25.
+//
+
+import Testing
+
+struct DiceViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
